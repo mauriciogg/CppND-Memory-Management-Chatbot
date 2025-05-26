c2769c02a6823130c82711340f0bdda4da9cd485
Mauricio Galindo <up.mauricio.g@gmail.com>
2025-05-25 20:28:45 -0700

    [Task 1] ChatBotPanelDialog has exlusive ownership of _chatLogic

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -117,8 +117,9 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     //// STUDENT CODE
     ////
 
+    // TASK 1: give ChatBotPanelDialog exlusive ownership of _chatLogic
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    _chatLogic = std::make_unique<ChatLogic>();
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -135,7 +136,9 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
+    // unique_ptr will handle this for us;
+    // TASK 1: give ChatBotPanelDialog exlusive ownership of _chatLogic
+    // delete _chatLogic;
 
     ////
     //// EOF STUDENT CODE
--- src/chatgui.h
+++ src/chatgui.h
@@ -2,6 +2,7 @@
 #define CHATGUI_H_
 
 #include <wx/wx.h>
+#include <memory>
 
 class ChatLogic; // forward declaration
 
@@ -16,7 +17,8 @@ private:
     //// STUDENT CODE
     ////
 
-    ChatLogic *_chatLogic;
+    // TASK 1: give ChatBotPanelDialog exlusive ownership of _chatLogic
+    std::unique_ptr<ChatLogic> _chatLogic;
 
     ////
     //// EOF STUDENT CODE
@@ -27,7 +29,14 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    ChatLogic *GetChatLogicHandle() {
+
+    std::unique_ptr<ChatLogic> _chatLogic;
+      // TASK 1: give ChatBotPanelDialog exlusive ownership of _chatLogic
+      // So much for exlusive ownership, but this should be ok since the only callers of
+      // this function live inside chatgui.
+      return _chatLogic.get();
+    }
 
     // events
     void paintEvent(wxPaintEvent &evt);

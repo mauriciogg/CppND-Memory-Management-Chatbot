5f131366caf9bd3741f8d0258be40ea1b4f2352b
Mauricio Galindo <up.mauricio.g@gmail.com>
2025-05-25 18:21:10 -0700

    [Task 0] Fix double delete bug

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,8 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    // we should not be deleting this guy
+    //delete _chatBot;
 
     ////
     //// EOF STUDENT CODE
